349a9866e0447c66b60335cdae10b35be4f2406f
Ty-Zeta <tyofzeta@gmail.com>
2017-06-29 10:35:27 -0400

    names

---
+++ Names.rb
@@ -0,0 +1,8 @@
+name = "Ty Cook"
+
+names = name.split
+firstname = names.first
+lastname = names.last
+
+p "My first name is #{firstname}"
+p "My last name is #{lastname}"
---
+++ name.rb
@@ -0,0 +1,8 @@
+def type1(f)
+	p "My name is #{f}"
+end
+def type2(l)
+	p "And my last name is #{l}"
+end
+type1("Ty")
+type2("Cook")
