6defa84bff87527612f2f5e43fe5bdbabc540e23
Ty-Zeta <tyofzeta@gmail.com>
2017-08-13 15:16:08 -0400

    updated with comments

--- Names.rb
+++ Names.rb
@@ -1,8 +1,15 @@
 name = "Ty Cook"
+# variable is set to string
 
 names = name.split
+# another variable is set to variable that is divide the elements in half
+
 firstname = names.first
+# variable is set to the first half of string
+
 lastname = names.last
+# variable is set to the last half of string
 
 p "My first name is #{firstname}"
 p "My last name is #{lastname}"
+# printed statement prints out each seperated half of string by calling them with #{ }
--- name.rb
+++ name.rb
@@ -1,8 +1,13 @@
 def type1(f)
 	p "My name is #{f}"
 end
+# function uses arguement f and then prints it
+
 def type2(l)
 	p "And my last name is #{l}"
 end
+# function uses arguement l and then prints it
+
 type1("Ty")
 type2("Cook")
+# the two functions are called and the value of the arguemnts are given so that they can be placed earlier in the code
